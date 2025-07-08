278957853e12d27382ff889470ba76303fb3aa2b
Sooraj Gupta <soorajgupta00@gmail.com>
2025-07-08 20:34:35 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonials";
 
 const page = () => {
   return (
-    <main className="bg-grid-white/[0.02] min-h-screen bg-black/[0.96] antialiased">
+    <main className="bg-grid-white/[0.2] min-h-screen bg-black/[0.96] antialiased">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 };
